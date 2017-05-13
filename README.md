95382715d9c79ba971e899f63e68e0fc9a7a6d24
Zigmas Satkevičius <zylius1@gmail.com>
2017-05-13 19:30:04 +0300

    Initial commit with api bundle.

---
+++ README.md
@@ -0,0 +1,4 @@
+GalerijaREST
+============
+
+A Symfony project created on May 13, 2017, 4:07 pm.
---
+++ src/GalerijaREST/ApiBundle/ApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GalerijaREST\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ApiBundle extends Bundle
+{
+}
